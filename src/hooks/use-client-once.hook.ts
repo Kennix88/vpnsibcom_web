698301fb7f6ab8e455cf0b5f85f6b0bd315f5b2f
Kennix88 <shel.live@yandex.ru>
2025-04-07 13:11:38 +0800

    add useDidMount hook

--- src/hooks/use-client-once.hook.ts
+++ src/hooks/use-client-once.hook.ts
@@ -1,6 +1,6 @@
 import { useRef } from 'react'
 
-export function useClientOnce(fn: () => void): void {
+export function useClientOnceHook(fn: () => void): void {
   const canCall = useRef(true)
   if (typeof window !== 'undefined' && canCall.current) {
     canCall.current = false
