b5a3a3c2d74cb1a56eb4f977208cdac743bf877f
Kennix88 <shel.live@yandex.ru>
2025-04-16 22:06:08 +0800

    New pages

---
+++ src/app/(tma)/tma/billing/page.tsx
@@ -0,0 +1,12 @@
+'use client'
+import { TmaPage } from '@app/app/(tma)/_components/TmaPage'
+
+export default function Page() {
+  return (
+    <TmaPage back={false}>
+      <div className="pt-4 px-4 pb-[80px] flex flex-col gap-4 text-[#fafafa]">
+        <h1>Billing </h1>
+      </div>
+    </TmaPage>
+  )
+}
