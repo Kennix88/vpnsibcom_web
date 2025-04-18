af76f6c77d4a8f5eda99adf9b4ea0f64c2845b06
Kennix88 <shel.live@yandex.ru>
2025-04-18 14:48:34 +0800

    add currency store

---
+++ src/store/currency.store.ts
@@ -0,0 +1,25 @@
+import { CurrencyInterface } from '@app/types/currency.interface'
+import { RatesInterface } from '@app/types/rates.interface'
+import { create } from 'zustand'
+import { persist } from 'zustand/middleware'
+
+interface CurrencyStore {
+  currencies: CurrencyInterface[]
+  rates: RatesInterface | null
+  setCurrencies: (currencies: CurrencyInterface[]) => void
+  setRates: (rates: RatesInterface) => void
+}
+
+export const useCurrencyStore = create<CurrencyStore>()(
+  persist(
+    (set) => ({
+      currencies: [],
+      rates: null,
+      setCurrencies: (currencies) => set({ currencies }),
+      setRates: (rates: RatesInterface) => set({ rates }),
+    }),
+    {
+      name: 'currency-storage',
+    },
+  ),
+)
