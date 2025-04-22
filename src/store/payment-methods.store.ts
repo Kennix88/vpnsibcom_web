1e2c8c2019e5ac0c649df0eda7bc7802ff813f71
Kennix88 <shel.live@yandex.ru>
2025-04-22 19:58:03 +0800

    create PaymentMethods Store

---
+++ src/store/payment-methods.store.ts
@@ -0,0 +1,20 @@
+import { PaymentMethodsDataInterface } from '@app/types/payment-methods-data.interface'
+import { create } from 'zustand'
+import { persist } from 'zustand/middleware'
+
+interface PaymentMethodsStore {
+  methods: PaymentMethodsDataInterface[]
+  setCurrencies: (methods: PaymentMethodsDataInterface[]) => void
+}
+
+export const usePaymentMethodsStore = create<PaymentMethodsStore>()(
+  persist(
+    (set) => ({
+      methods: [],
+      setCurrencies: (methods) => set({ methods }),
+    }),
+    {
+      name: 'payment-methods-storage',
+    },
+  ),
+)
