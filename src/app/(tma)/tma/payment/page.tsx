26c1f5a836e30dc064207bbab9df132eac59e6e7
Kennix88 <shel.live@yandex.ru>
2025-04-29 11:04:13 +0800

    update payment page

--- src/app/(tma)/tma/payment/page.tsx
+++ src/app/(tma)/tma/payment/page.tsx
@@ -1,11 +1,42 @@
 'use client'
 import { TmaPage } from '@app/app/(tma)/_components/TmaPage'
+import Payments from '@app/app/_components/payments/Payments'
+import { authApiClient } from '@app/core/authApiClient'
+import { usePaymentMethodsStore } from '@app/store/payment-methods.store'
+import { useUserStore } from '@app/store/user.store'
+import { useEffect } from 'react'
+import { toast } from 'react-toastify'
+import { useTranslations } from 'use-intl'
 
 export default function Page() {
+  const t = useTranslations('billing.payment')
+  const { setUser } = useUserStore()
+  const { setMethods } = usePaymentMethodsStore()
+
+  useEffect(() => {
+    const getMethods = async () => {
+      try {
+        const updated = await authApiClient.getPaymentMethods(true)
+        setMethods(updated.methods)
+        setUser(updated.user)
+      } catch {
+        toast.error('Error updating data')
+      }
+    }
+    getMethods()
+    return () => {}
+  }, [])
+
   return (
     <TmaPage back={true}>
-      <div className="pt-4 px-4 pb-[80px] flex flex-col gap-4 text-[#fafafa]">
-        <h1>Payment</h1>
+      <div className="flex flex-row gap-4 flex-wrap justify-center">
+        <div className="flex flex-col gap-2 uppercase font-mono w-full">
+          <div className="text-2xl font-bold ">{t('title')}</div>
+          {/*<div className="text-md font-bold font-mono">*/}
+          {/*  {t('get')} <span className="opacity-80 font-normal">{t('for')}</span>*/}
+          {/*</div>*/}
+        </div>
+        <Payments isTma={true} />
       </div>
     </TmaPage>
   )
