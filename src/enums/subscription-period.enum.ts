62ad162a9e9425271f610c2d57afa977d6924693
Kennix88 <shel.live@yandex.ru>
2025-05-02 20:39:15 +0800

    feat: add SubscriptionPeriodEnum to define subscription periods
    This enum is introduced to standardize the representation of subscription periods across the application, ensuring consistency and reducing the risk of errors.

---
+++ src/enums/subscription-period.enum.ts
@@ -0,0 +1,11 @@
+export enum SubscriptionPeriodEnum {
+  TRIAL = 'TRIAL',
+  HOUR = 'HOUR',
+  DAY = 'DAY',
+  MONTH = 'MONTH',
+  THREE_MONTH = 'THREE_MONTH',
+  SIX_MONTH = 'SIX_MONTH',
+  YEAR = 'YEAR',
+  TWO_YEAR = 'TWO_YEAR',
+  THREE_YEAR = 'THREE_YEAR',
+}
