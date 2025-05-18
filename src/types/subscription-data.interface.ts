67908ba70a07f376a45bec189dd414c36812c24a
Kennix88 <shel.live@yandex.ru>
2025-05-18 16:33:17 +0800

    refactor(types): rename and expand subscription data interfaces
    The `SubscriptionDataInterface` has been renamed to `SubscriptionResponseInterface` and significantly expanded to include additional fields related to subscription pricing and details. A new `SubscriptionDataInterface` has been introduced to encapsulate detailed subscription information, and a `ServerDataInterface` has been added to describe server-related data. These changes improve the clarity and maintainability of the subscription-related types.

--- src/types/subscription-data.interface.ts
+++ src/types/subscription-data.interface.ts
@@ -1,24 +1,65 @@
 import { SubscriptionPeriodEnum } from '@app/enums/subscription-period.enum'
 
-export interface SubscriptionDataInterface {
-  priceSubscriptionStars: number
+export interface SubscriptionResponseInterface {
+  telegramPremiumRatio: number
+  devicesPriceStars: number
+  serversPriceStars: number
+  premiumServersPriceStars: number
+  allServersPriceStars: number
+  allPremiumServersPriceStars: number
+  trafficGbPriceStars: number
+  unlimitTrafficPriceStars: number
   hourRatioPayment: number
   dayRatioPayment: number
+  weekRatioPayment: number
   threeMouthesRatioPayment: number
   sixMouthesRatioPayment: number
   oneYearRatioPayment: number
   twoYearRatioPayment: number
   threeYearRatioPayment: number
-  list: SubscriptionDataListInterface[]
+  indefinitelyRatio: number
+  fixedPriceStars: number
+  subscriptions: SubscriptionDataInterface[]
 }
 
-export interface SubscriptionDataListInterface {
+export interface SubscriptionDataInterface {
   id: string
   period: SubscriptionPeriodEnum
+  periodMultiplier: number
   isActive: boolean
   isAutoRenewal: boolean
+  nextRenewalStars?: number
+  isFixedPrice: boolean
+  fixedPriceStars?: number
+  devicesCount: number
+  isAllServers: boolean
+  isAllPremiumServers: boolean
+  trafficLimitGb?: number
+  isUnlimitTraffic: boolean
+
+  lastUserAgent?: string
+  dataLimit?: number
+  usedTraffic: number
+  lifeTimeUsedTraffic: number
+  links: string[]
+
+  servers: ServerDataInterface[]
+
   createdAt: Date
   updatedAt: Date
   expiredAt?: Date
+  onlineAt?: Date
+
+  token: string
   subscriptionUrl: string
+}
+
+export interface ServerDataInterface {
+  code: string
+  name: string
+  flagKey: string
+  flagEmoji: string
+  network: number
+  isActive: boolean
+  isPremium: boolean
 }
