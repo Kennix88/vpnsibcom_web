e34ffebb1d73de4e8c5e8941adc09637aaf7129b
Kennix88 <shel.live@yandex.ru>
2025-05-22 10:16:15 +0800

    feat(subscription-period): add WEEK and INDEFINITELY periods
    This commit introduces two new subscription periods, WEEK and INDEFINITELY, to the SubscriptionPeriodEnum. These additions expand the range of subscription options available in the system.

--- src/enums/subscription-period.enum.ts
+++ src/enums/subscription-period.enum.ts
@@ -2,10 +2,12 @@ export enum SubscriptionPeriodEnum {
   TRIAL = 'TRIAL',
   HOUR = 'HOUR',
   DAY = 'DAY',
+  WEEK = 'WEEK',
   MONTH = 'MONTH',
   THREE_MONTH = 'THREE_MONTH',
   SIX_MONTH = 'SIX_MONTH',
   YEAR = 'YEAR',
   TWO_YEAR = 'TWO_YEAR',
   THREE_YEAR = 'THREE_YEAR',
+  INDEFINITELY = 'INDEFINITELY',
 }
