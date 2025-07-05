d8cd6546a03f94693ae94569854cf26353c792d7
Kennix88 <shel.live@yandex.ru>
2025-07-06 00:38:42 +0800

    feat(subscription): add platform enum for subscription types

---
+++ src/app/_components/subscription/types/platform.enum.ts
@@ -0,0 +1,9 @@
+export enum PlatformEnum {
+  ANDROID = 'ANDROID',
+  ANDROID_TV = 'ANDROID_TV',
+  IOS = 'IOS',
+  APPLE_TV = 'APPLE_TV',
+  WINDOWS = 'WINDOWS',
+  LINUX = 'LINUX',
+  MACOS = 'MACOS',
+}
