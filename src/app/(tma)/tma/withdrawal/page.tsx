e18b4d7680b85c4f4437591ece0c8064ec1de46f
Kennix88 <shel.live@yandex.ru>
2025-07-08 23:53:33 +0800

    style(tma/withdrawal): update withdrawal page styling and add dev notice
    Update font styling and add development notice to withdrawal page. The changes improve visual consistency and inform users that the feature is in progress.

--- src/app/(tma)/tma/withdrawal/page.tsx
+++ src/app/(tma)/tma/withdrawal/page.tsx
@@ -4,8 +4,9 @@ import { TmaPage } from '@app/app/(tma)/_components/TmaPage'
 export default function Page() {
   return (
     <TmaPage back={true}>
-      <div className="pt-4 px-4 pb-[80px] flex flex-col gap-4 text-[#fafafa]">
-        <h1>Withdrawal</h1>
+      <div className="pt-4 px-4 pb-[80px] flex flex-col gap-4 font-mono font-bold">
+        <h1 className="text-xl">Withdrawal</h1>
+        <div>In development...</div>
       </div>
     </TmaPage>
   )
