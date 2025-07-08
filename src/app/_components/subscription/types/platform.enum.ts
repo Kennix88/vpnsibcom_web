807477254875864acac2928960749d21b7e888bb
Kennix88 <shel.live@yandex.ru>
2025-07-08 16:05:48 +0800

    feat(subscription): add IPADOS to platform enum

--- src/app/_components/subscription/types/platform.enum.ts
+++ src/app/_components/subscription/types/platform.enum.ts
@@ -2,6 +2,7 @@ export enum PlatformEnum {
   ANDROID = 'ANDROID',
   ANDROID_TV = 'ANDROID_TV',
   IOS = 'IOS',
+  IPADOS = 'IPADOS',
   APPLE_TV = 'APPLE_TV',
   WINDOWS = 'WINDOWS',
   LINUX = 'LINUX',
