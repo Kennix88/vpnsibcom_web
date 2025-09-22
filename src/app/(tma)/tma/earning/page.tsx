02eb33eea3cbc651afdef369a4232724ee02b2ad
Kennix88 <shel.live@yandex.ru>
2025-09-22 20:45:05 +0800

    feat(tma/earning): add TopBar and update styling in earnings page
    Add TopBar component and improve text styling with larger font sizes and monospace font for the "Soon..." message

--- src/app/(tma)/tma/earning/page.tsx
+++ src/app/(tma)/tma/earning/page.tsx
@@ -1,11 +1,14 @@
 'use client'
 import { TmaPage } from '@app/app/(tma)/_components/TmaPage'
+import TopBar from '@app/app/_components/TopBar'
 
 export default function Page() {
   return (
     <TmaPage back={false}>
-      <div className="pt-4 px-4 pb-[80px] flex flex-col gap-4 text-[#fafafa]">
-        <h1>Earning </h1>
+      <TopBar />
+      <div className="pt-4 px-4 pb-[80px] flex flex-col gap-2">
+        <h1 className="text-2xl font-bold uppercase">Earning</h1>
+        <div className="text-lg font-bold font-mono">Soon...</div>
       </div>
     </TmaPage>
   )
