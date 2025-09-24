037f755c772f4cd55311dc99ffd3acb4f8c110cf
Kennix88 <shel.live@yandex.ru>
2025-09-25 03:39:12 +0800

    refactor(Version): change layout to flex-col and remove GitHub link
    The component layout was simplified by switching to a column flex direction and removing the commented-out GitHub link section which was no longer needed.

--- src/app/_components/Version.tsx
+++ src/app/_components/Version.tsx
@@ -1,21 +1,20 @@
 'use client'
 
 import { config } from '@app/config/client'
-import { Github } from 'lucide-react'
 import Link from 'next/link'
 
 export default function Version() {
   return (
-    <div className="font-mono flex-wrap flex gap-2 items-center justify-center">
+    <div className="font-mono flex-col flex gap-2 items-center justify-center">
       <div>v{config.APP_VERSION}</div>
-      <div className="text-[12px]">x</div>
+      {/* <div className="text-[12px]">x</div>
       <Link
         href={config.GITHUBREPO_URL || ''}
         target="_blank"
         className="flex gap-2 items-center text-[var(--primary)] transition-all duration-200 hover:brightness-110 active:scale-[0.97] cursor-pointer">
         <Github size={18} />
-        Open-Source
-      </Link>
+        GitHub
+      </Link> */}
       <div className="flex gap-2 items-center justify-center ">
         <div className="text-nowrap">Developed by</div>
         <Link
