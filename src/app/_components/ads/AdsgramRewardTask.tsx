7588fc524257ee9d2670d49798ea6647218f7292
Kennix88 <shel.live@yandex.ru>
2025-11-19 15:59:28 +0800

    feat(ads): add AdsgramRewardTask component for ad rewards
    implement component to fetch and display ads with rewards, handle reward confirmation and error states

---
+++ src/app/_components/ads/AdsgramRewardTask.tsx
@@ -0,0 +1,121 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+'use client'
+import { BlockId } from '@adsgram/common'
+import { AdsgramTask } from '@adsgram/react'
+import { authApiClient } from '@app/core/authApiClient'
+import { AdsPlaceEnum } from '@app/enums/ads-place.enum'
+import { AdsResInterface } from '@app/enums/ads-res.interface'
+import { AdsTaskTypeEnum } from '@app/enums/ads-task-type.enum'
+import { useUserStore } from '@app/store/user.store'
+import { useTranslations } from 'next-intl'
+import { JSX, useCallback, useEffect, useRef, useState } from 'react'
+import { HiGift } from 'react-icons/hi'
+import { MdDoubleArrow, MdFileDownloadDone } from 'react-icons/md'
+import { toast } from 'react-toastify'
+import Currency from '../Currency'
+
+export function AdsgramRewardTask() {
+  const [loading, setLoading] = useState(false)
+  const [ad, setAd] = useState<AdsResInterface | null>(null)
+  const { setUser } = useUserStore()
+  const taskRef = useRef<JSX.IntrinsicElements['adsgram-task']>(null)
+  const t = useTranslations('earning')
+
+  const fetchAd = useCallback(async (): Promise<void> => {
+    try {
+      setLoading(true)
+      const response = await authApiClient.getAds(
+        AdsPlaceEnum.TASK,
+        AdsTaskTypeEnum.TASK,
+      )
+      setAd(response)
+    } catch (error) {
+      console.error('Failed to load ad', error)
+      // toast.error(t('errors.loadFailed'))
+    } finally {
+      setLoading(false)
+    }
+  }, [setAd])
+
+  useEffect(() => {
+    fetchAd()
+  }, [fetchAd])
+
+  const handleReward = useCallback(
+    async (event: CustomEvent<string>): Promise<void> => {
+      try {
+        setLoading(true)
+        const response = await authApiClient.confirmAds(ad?.verifyKey as string)
+        setUser(response.user)
+        if (response.success) {
+          toast.success(t('earned'))
+        }
+      } catch (error) {
+        console.error('Failed to load ad', error)
+        //
+      } finally {
+        setLoading(false)
+      }
+    },
+    [setLoading, ad, setUser, t],
+  )
+
+  const handleError = (event: CustomEvent<string>): void => {
+    console.error('Task error:', event.detail)
+  }
+
+  if (!ad) {
+    return null
+  }
+
+  // if (!customElements.get('adsgram-task')) {
+  //   return null
+  // }
+
+  return (
+    <AdsgramTask
+      blockId={ad.blockId as BlockId}
+      debug={false}
+      className="adsgram-task p-2 bg-[var(--surface-container-lowest)] rounded-md font-bold"
+      // @ts-expect-error
+      ref={taskRef}
+      onReward={handleReward}
+      onError={handleError}>
+      <span slot="reward" className="text-[12px] mt-2 flex gap-2 flex-wrap">
+        {ad.rewards.stars > 0 && (
+          <div className="inline-flex gap-1 items-center px-2 py-1 rounded-md bg-[var(--star-container-rgba)] w-fit">
+            <Currency w={18} type={'star'} />
+            {ad.rewards.stars}
+          </div>
+        )}
+        {ad.rewards.traffic > 0 && (
+          <div className="inline-flex gap-1 items-center px-2 py-1 rounded-md bg-[var(--traffic-container-rgba)] w-fit">
+            <Currency w={18} type={'traffic'} />
+            {ad.rewards.traffic}
+          </div>
+        )}
+        {ad.rewards.tickets > 0 && (
+          <div className="inline-flex gap-1 items-center px-2 py-1 rounded-md bg-[var(--ticket-container-rgba)] w-fit">
+            <Currency w={18} type={'ticket'} />
+            {ad.rewards.tickets}
+          </div>
+        )}
+      </span>
+      <div
+        slot="button"
+        className="font-medium flex items-center justify-center bg-[var(--primary)] text-[var(--on-primary)] px-2 py-1 rounded-md ml-2 uppercase cursor-pointer">
+        <MdDoubleArrow size={18} />
+      </div>
+      <div
+        slot="claim"
+        className="font-medium flex items-center justify-center bg-[var(--warning)] text-[var(--on-warning)] px-2 py-1 rounded-md ml-2 uppercase cursor-pointer">
+        <HiGift size={18} />
+      </div>
+      <div
+        slot="done"
+        className="font-medium flex items-center justify-center bg-[var(--success)] text-[var(--on-success)] px-2 py-1 rounded-md ml-2 uppercase cursor-not-allowed">
+        <MdFileDownloadDone size={18} />
+      </div>
+    </AdsgramTask>
+  )
+}
