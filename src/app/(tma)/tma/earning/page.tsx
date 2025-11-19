4641887cb303b63392f1873e3e339d461ecd529e
Kennix88 <shel.live@yandex.ru>
2025-11-19 16:00:01 +0800

    feat(earning): add ads reward components and translations
    - Replace placeholder content with TaskAdsReward and AdsgramRewardTask components
    - Add translations for the earning page title

--- src/app/(tma)/tma/earning/page.tsx
+++ src/app/(tma)/tma/earning/page.tsx
@@ -1,20 +1,24 @@
 'use client'
 import { TmaPage } from '@app/app/(tma)/_components/TmaPage'
+import { AdsgramRewardTask } from '@app/app/_components/ads/AdsgramRewardTask'
+import { TaskAdsReward } from '@app/app/_components/ads/TaskAdsReward'
 import SocialButtons from '@app/app/_components/SocialButtons'
 import TopBar from '@app/app/_components/TopBar'
 import Version from '@app/app/_components/Version'
+import { useTranslations } from 'next-intl'
 
 export default function Page() {
+  const t = useTranslations('earning')
   return (
     <TmaPage back={false}>
       <TopBar />
       <div className="pt-4 px-4  flex flex-col gap-4">
-        <h1 className="text-2xl font-bold uppercase">Earning</h1>
-        <div className="text-lg font-bold font-mono">Soon...</div>
+        <h1 className="text-2xl font-bold uppercase">{t('title')}</h1>
+        {/* <div className="text-lg font-bold font-mono">Soon...</div> */}
       </div>
       <div className="flex flex-col gap-4 mt-4 pb-[80px]">
-        {/* <ShowAdButton />
-        <AsdgramTask blockId="task-17144" /> */}
+        <TaskAdsReward />
+        <AdsgramRewardTask />
       </div>
       <div className="flex flex-col gap-4">
         <SocialButtons />
