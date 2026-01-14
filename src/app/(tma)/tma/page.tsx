c85563d9909c2dd77ac9478a7885903ce2be84fd
Kennix88 <shel.live@yandex.ru>
2026-01-14 17:58:07 +0800

    refactor(tma): remove AdsonarBanner component from tma page
    The AdsonarBanner component and its related imports were removed as they are no longer needed in the TMA page layout. This simplifies the component structure and removes unused code.

--- src/app/(tma)/tma/page.tsx
+++ src/app/(tma)/tma/page.tsx
@@ -1,13 +1,11 @@
 'use client'
 import { TmaPage } from '@app/app/(tma)/_components/TmaPage'
-import AdsonarBanner from '@app/app/_components/ads/AdsonarBanner'
 import { FreePlanActivated } from '@app/app/_components/FreePlanAtivated'
 import GreenCheck from '@app/app/_components/GreenCheck'
 import SocialButtons from '@app/app/_components/SocialButtons'
 import { Subscriptions } from '@app/app/_components/Subscriptions'
 import TopBar from '@app/app/_components/TopBar'
 import Version from '@app/app/_components/Version'
-import { AdsPlaceEnum } from '@app/enums/ads-place.enum'
 
 export default function Page() {
   return (
@@ -15,7 +13,6 @@ export default function Page() {
       <div className="flex flex-col gap-4 items-center">
         <TopBar />
         <GreenCheck />
-        <AdsonarBanner place={AdsPlaceEnum.BANNER} />
         <FreePlanActivated />
         <Subscriptions />
         <br />
