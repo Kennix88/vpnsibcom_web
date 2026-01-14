8c09e5718577d62f66a1c3f3c95539475b50da2f
Kennix88 <shel.live@yandex.ru>
2026-01-14 17:57:24 +0800

    refactor(tma/games): remove unused ad components from games page
    Clean up the games page by removing unused AdsonarBanner component and related AdsPlaceEnum import that are no longer needed.

--- src/app/(tma)/tma/games/page.tsx
+++ src/app/(tma)/tma/games/page.tsx
@@ -1,10 +1,8 @@
 'use client'
 import { TmaPage } from '@app/app/(tma)/_components/TmaPage'
-import AdsonarBanner from '@app/app/_components/ads/AdsonarBanner'
 import SocialButtons from '@app/app/_components/SocialButtons'
 import TopBar from '@app/app/_components/TopBar'
 import Version from '@app/app/_components/Version'
-import { AdsPlaceEnum } from '@app/enums/ads-place.enum'
 
 export default function Page() {
   return (
@@ -13,7 +11,6 @@ export default function Page() {
       <div className="pt-4 px-4 pb-[80px] flex flex-col gap-2">
         <h1 className="text-2xl font-bold uppercase">Games</h1>
         <div className="text-lg font-bold font-mono">Soon...</div>
-        <AdsonarBanner place={AdsPlaceEnum.BANNER_4} />
       </div>
       <div className="flex flex-col gap-4">
         <SocialButtons />
