a33373a26290d353bf53b21d6232ef5c65e14e08
Kennix88 <shel.live@yandex.ru>
2026-01-14 17:57:54 +0800

    refactor(subscription): remove unused ad components from subscription page
    Clean up the subscription page by removing unused ad-related imports and components to simplify the codebase and improve maintainability

--- src/app/(tma)/tma/subscription/[id]/page.tsx
+++ src/app/(tma)/tma/subscription/[id]/page.tsx
@@ -1,10 +1,8 @@
 import { TmaPage } from '@app/app/(tma)/_components/TmaPage'
-import AdsonarBanner from '@app/app/_components/ads/AdsonarBanner'
 import SocialButtons from '@app/app/_components/SocialButtons'
 import Subscription from '@app/app/_components/subscription/Subscription'
 import TopBar from '@app/app/_components/TopBar'
 import Version from '@app/app/_components/Version'
-import { AdsPlaceEnum } from '@app/enums/ads-place.enum'
 
 export default async function Page({
   params,
@@ -16,7 +14,6 @@ export default async function Page({
     <TmaPage back={true}>
       <TopBar />
       <Subscription token={id} isToken={false} />
-      <AdsonarBanner place={AdsPlaceEnum.BANNER_7} />
       <div className="flex flex-col gap-4">
         <SocialButtons />
         <Version />
