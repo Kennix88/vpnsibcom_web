a1f38f67f910a7e508c812cda0d353818387bcaa
Kennix88 <shel.live@yandex.ru>
2026-02-11 17:55:11 +0800

    feat(tma): integrate fullscreen ads and reward component
    Add useFullscreenAd hook and TaskAdsReward component to TMA page to enable ad monetization and reward functionality.

--- src/app/(tma)/tma/page.tsx
+++ src/app/(tma)/tma/page.tsx
@@ -1,5 +1,7 @@
 'use client'
 import { TmaPage } from '@app/app/(tma)/_components/TmaPage'
+import { TaskAdsReward } from '@app/app/_components/ads/TaskAdsReward'
+import { useFullscreenAd } from '@app/app/_components/ads/useFullscreenAd'
 import { FreePlanActivated } from '@app/app/_components/FreePlanAtivated'
 import GreenCheck from '@app/app/_components/GreenCheck'
 import SocialButtons from '@app/app/_components/SocialButtons'
@@ -8,12 +10,15 @@ import TopBar from '@app/app/_components/TopBar'
 import Version from '@app/app/_components/Version'
 
 export default function Page() {
+  useFullscreenAd()
   return (
     <TmaPage back={false}>
       <div className="flex flex-col gap-4 items-center">
         <TopBar />
         <GreenCheck />
         <FreePlanActivated />
+        <TaskAdsReward />
+
         <Subscriptions />
         <br />
         <SocialButtons />
