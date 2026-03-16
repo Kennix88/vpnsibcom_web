2c1fd610abc9000155d150d22682666571ccbf9f
Kennix88 <shel.live@yandex.ru>
2026-03-16 22:12:59 +0800

    refactor(referrals): Simplify referral data interface
    Removes redundant fields and renames fields to be more specific.
    This change consolidates several invite-related fields and
    clarifies the naming of rewarded amounts to use USDT.

--- src/types/referrals-data-interface.ts
+++ src/types/referrals-data-interface.ts
@@ -1,38 +1,13 @@
 export interface ReferralsDataInterface {
-  inviteBotUrl: string
-  inviteBotTgDeeplink: string
-  inviteTmaUrl: string
-  inviteTmaTgDeeplink: string
-  inviteAppUrl: string
-  inviteAppTgDeeplink: string
-  lvl1IsActivated: number
-  lvl2IsActivated: number
-  lvl3IsActivated: number
-  lvl1IsActivatedPremium: number
-  lvl2IsActivatedPremium: number
-  lvl3IsActivatedPremium: number
-  lvl1IsActivatedBase: number
-  lvl2IsActivatedBase: number
-  lvl3IsActivatedBase: number
-  lvl1TotalPaymentsRewarded: number
-  lvl2TotalPaymentsRewarded: number
-  lvl3TotalPaymentsRewarded: number
-  lvl1TotalTrafficRewarded: number
-  lvl2TotalTrafficRewarded: number
-  lvl3TotalTrafficRewarded: number
+  lvl1TotalUsdtRewarded: number
+  lvl2TotalUsdtRewarded: number
+  lvl3TotalUsdtRewarded: number
   lvl1Percent: number
   lvl2Percent: number
   lvl3Percent: number
-  inviteReward: number
-  invitePremiumReward: number
-  inviteFriendReward: number
-  invitePremiumFriendReward: number
   lvl1Count: number
   lvl2Count: number
   lvl3Count: number
-  lvl1List: ReferralDataInterface[]
-  lvl2List: ReferralDataInterface[]
-  lvl3List: ReferralDataInterface[]
 }
 
 export interface ReferralDataInterface {
@@ -42,6 +17,5 @@ export interface ReferralDataInterface {
   fullName: string
   username?: string
   photoUrl?: string
-  totalPaymentsRewarded: number
-  totalTrafficRewarded: number
+  totalUsdtRewarded: number
 }
