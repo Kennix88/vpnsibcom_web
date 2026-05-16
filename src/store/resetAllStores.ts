1e29816ee15e81152b2d2d8e0550880bab09fcbc
Kennix88 <shel.live@yandex.ru>
2026-05-17 03:48:12 +0800

    update

---
+++ src/store/resetAllStores.ts
@@ -0,0 +1,33 @@
+import { useCurrencyStore } from '@app/store/currency.store'
+import { usePaymentMethodsStore } from '@app/store/payment-methods.store'
+import { usePlansStore } from '@app/store/plans.store'
+import { useRefferlsStore } from '@app/store/referrals.store'
+import { useServersStore } from '@app/store/servers.store'
+import { useSubscriptionsStore } from '@app/store/subscriptions.store'
+import { useUserStore } from '@app/store/user.store'
+
+const STORE_KEYS = [
+  'user-storage',
+  'currency-storage',
+  'payment-methods-storage',
+  'plans-storage',
+  'referrals-storage',
+  'servers-storage',
+  'subscriptions-storage',
+]
+
+export function resetAllStores() {
+  useUserStore.setState({ user: null, accessToken: null })
+  useCurrencyStore.setState({ currencies: [], rates: null })
+  usePaymentMethodsStore.setState({ methods: [] })
+  usePlansStore.setState({ plansData: null })
+  useRefferlsStore.setState({ referralsData: null })
+  useServersStore.setState({ serversData: null })
+  useSubscriptionsStore.setState({ subscriptions: null })
+
+  if (typeof window !== 'undefined') {
+    for (const key of STORE_KEYS) {
+      window.localStorage.removeItem(key)
+    }
+  }
+}
