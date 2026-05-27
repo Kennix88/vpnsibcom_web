d7d942ebcea9f89e20664230bae04c36c5c1e917
Kennix88 <shel.live@yandex.ru>
2026-05-27 21:14:08 +0800

    update

--- src/app/(tma)/tma/layout.tsx
+++ src/app/(tma)/tma/layout.tsx
@@ -1,6 +1,7 @@
 import { I18nProvider } from '@app/core/i18n/provider'
 import { TelegramProvider } from '@app/providers/TelegramProvider'
 import { Metadata } from 'next'
+import Script from 'next/script'
 import { getLocale } from 'next-intl/server'
 // import 'normalize.css/normalize.css'
 import { CheckPlatform } from '@app/app/(tma)/tma/_components/CheckPlatform'
@@ -9,6 +10,29 @@ import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
 import '../../_assets/globals.css'
 import { InitData } from '../_components/InitData'
+
+const telegramDesktopBridgePatch = `
+;(function () {
+  var w = window;
+  var noop = function () {};
+
+  w.TelegramGameProxy = w.TelegramGameProxy || {};
+  if (typeof w.TelegramGameProxy.receiveEvent !== 'function') {
+    w.TelegramGameProxy.receiveEvent = noop;
+  }
+
+  w.Telegram = w.Telegram || {};
+  w.Telegram.WebView = w.Telegram.WebView || {};
+  if (typeof w.Telegram.WebView.receiveEvent !== 'function') {
+    w.Telegram.WebView.receiveEvent = noop;
+  }
+
+  if (typeof w.TelegramGameProxy_receiveEvent !== 'function') {
+    w.TelegramGameProxy_receiveEvent = function () {
+      return w.TelegramGameProxy.receiveEvent.apply(w.TelegramGameProxy, arguments);
+    };
+  }
+})();`
 
 export const metadata: Metadata = {
   title: 'TMA - VPNsib',
@@ -23,6 +47,13 @@ export default async function TmaLayout({
   const locale = await getLocale()
   return (
     <html lang={locale} className={'dark'}>
+      <head>
+        <Script
+          id="telegram-desktop-bridge-patch"
+          strategy="beforeInteractive"
+          dangerouslySetInnerHTML={{ __html: telegramDesktopBridgePatch }}
+        />
+      </head>
       <body className="bg-[var(--background)]">
         <I18nProvider>
           <TelegramProvider>
