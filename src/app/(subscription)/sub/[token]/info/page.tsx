ee6ef2c47812b94e3ec17d3b85ccb9217f592797
Kennix88 <shel.live@yandex.ru>
2026-05-30 18:53:05 +0800

    update

--- src/app/(subscription)/sub/[token]/info/page.tsx
+++ src/app/(subscription)/sub/[token]/info/page.tsx
@@ -1,6 +1,4 @@
-import SocialButtons from '@app/app/_components/SocialButtons'
 import Subscription from '@app/app/_components/subscription/Subscription'
-import Version from '@app/app/_components/Version'
 
 export default async function Page({
   params,
@@ -9,10 +7,10 @@ export default async function Page({
 }) {
   const { token } = await params
   return (
-    <div className="flex flex-col gap-4 p-4 items-center font-extralight font-mono w-full">
-      <Subscription token={token} isToken={true} />
-      <SocialButtons />
-      <Version />
+    <div className="w-full flex justify-center">
+      <div className="max-w-md w-full flex flex-col items-stretch ">
+        <Subscription token={token} isToken={true} />
+      </div>
     </div>
   )
 }
