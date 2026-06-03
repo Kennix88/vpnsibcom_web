fad13676d141060b2dd85cd03b5520c691adafd0
Kennix88 <shel.live@yandex.ru>
2026-06-04 01:14:24 +0800

    update

---
+++ src/app/(ads)/ad-redirect/[key]/layout.tsx
@@ -0,0 +1,39 @@
+import { I18nProvider } from '@app/core/i18n/provider'
+import { getLocale } from 'next-intl/server'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+import '../../../_assets/globals.css'
+
+export const metadata = {
+  title: 'Переход к рекламному партнеру - VPNsib',
+  description: 'Переход к рекламному партнеру',
+}
+
+export default async function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  const locale = await getLocale()
+  return (
+    <html lang={locale}>
+      <body className="bg-[var(--background)]">
+        <I18nProvider>
+          <ToastContainer
+            position="bottom-center"
+            autoClose={3000}
+            hideProgressBar={false}
+            newestOnTop
+            closeOnClick
+            rtl={false}
+            pauseOnFocusLoss
+            draggable
+            pauseOnHover
+            theme="dark"
+          />
+          {children}
+        </I18nProvider>
+      </body>
+    </html>
+  )
+}
