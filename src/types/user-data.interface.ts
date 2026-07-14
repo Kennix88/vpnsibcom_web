05b76327ed454b5dbcb57ece7857a954f1b6f1ee
Kennix88 <shel.live@yandex.ru>
2026-07-15 01:52:23 +0800

    Update user-data.interface.ts

--- src/types/user-data.interface.ts
+++ src/types/user-data.interface.ts
@@ -4,16 +4,14 @@ import { UserRolesEnum } from '@app/enums/user-roles.enum'
 export interface UserDataInterface {
   id: string
   telegramId: string
-  isFreePlanAvailable: boolean
-  trialGb?: number
   isBanned: boolean
   isDeleted: boolean
   banExpiredAt?: Date
+  premiumExpiredAt?: Date
   deletedAt?: Date
   role: UserRolesEnum
   roleName: string
   roleDiscount: number
-  limitSubscriptions: number
   isPremium: boolean
   fullName: string
   username?: string
@@ -30,10 +28,44 @@ export interface UserDataInterface {
   nextAdsgramTaskAt?: Date
   minPayStars: number
   lastFullscreenViewedAt?: Date
+  premium: PremiumStatusInterface
 }
 
 export interface UserBalanceInterface {
   payment: number
   hold: number
   usdt: number
+}
+
+export interface PremiumStatusInterface {
+  methods: PremiumStatusMethodInterface[]
+  periods: PremiumStatusPeriodInterface[]
+}
+
+export interface PremiumStatusMethodInterface {
+  method: PayPremiumMethodsEnum // Стиль кнопки зависит от метода
+  price: number // Изначальная цена без скидок
+  finalPrice: number // Финальная цена с учетом скидок доступных пользователю (без учета скидки за период) (например за роль или общая скидка для всех на премиум сейчас действует). Нужно визуализировать выгоду
+  icon: string // ключ иконки для компонента Currency
+}
+
+export interface PremiumStatusPeriodInterface {
+  period: PayPremiumPeriodEnum
+  name: string
+  discount: number // Дополнительный множитель скидки (пример 0,8 - 20% доп. скидки) за период, применить к финальной цене при визуализации (визуализировать скидку на кнопках выбора периода)
+}
+
+export enum PayPremiumMethodsEnum {
+  BALANCE_STARS = 'BALANCE_STARS', // (STARS это payment баланс) Блокировать кнопку если баланса не достаточно
+  BALANCE_USDT = 'BALANCE_USDT', // (USDT это usdt баланс) Блокировать кнопку если баланса не достаточно
+}
+
+export enum PayPremiumPeriodEnum {
+  MONTH = 'MONTH',
+  THREE_MONTH = 'THREE_MONTH',
+  SIX_MONTH = 'SIX_MONTH',
+  YEAR = 'YEAR',
+  TWO_YEAR = 'TWO_YEAR',
+  THREE_YEAR = 'THREE_YEAR',
+  INDEFINITELY = 'INDEFINITELY', // Пожизненно
 }
