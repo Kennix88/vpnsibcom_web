3c85dbfdc013970e15033ced0898298eccd62237
Kennix88 <shel.live@yandex.ru>
2026-07-26 00:17:57 +0800

    Update page.tsx

--- src/app/(tma)/tma/page.tsx
+++ src/app/(tma)/tma/page.tsx
@@ -1,23 +1,12 @@
 'use client'
 import { TmaPage } from '@app/app/(tma)/_components/TmaPage'
-import { TaskAdsgramTask } from '@app/app/_components/ads/TaskAdsgram'
 import { useFullscreenAd } from '@app/app/_components/ads/useFullscreenAd'
-import { Extensions } from '@app/app/_components/Extensions'
 import { PremiumCTA } from '@app/app/_components/PremiumCTA'
 import SocialButtons from '@app/app/_components/SocialButtons'
 import { Subscription } from '@app/app/_components/subscription/Subscription'
 import TopBar from '@app/app/_components/TopBar'
-import dynamic from 'next/dynamic'
-
-const TaskAdsReward = dynamic(
-  () =>
-    import('@app/app/_components/ads/TaskAdsReward').then(
-      (mod) => mod.TaskAdsReward,
-    ),
-  {
-    ssr: false,
-  },
-)
+import { Users2 } from 'lucide-react'
+import { SectionHeading } from './earning/_components/SectionHeading'
 
 export default function Page() {
   useFullscreenAd()
@@ -27,12 +16,17 @@ export default function Page() {
         <TopBar />
         <PremiumCTA />
         <Subscription />
-        <TaskAdsReward />
-        <TaskAdsgramTask debug={process.env.NODE_ENV !== 'production'} />
-        <Extensions />
+
         {/*<SupportBanner />*/}
         <br />
-        <SocialButtons />
+        <div className="flex flex-col gap-2.5">
+          <SectionHeading
+            icon={<Users2 size={14} />}
+            title="Сообщество"
+            hint="Будь на связи с VPNsib"
+          />
+          <SocialButtons />
+        </div>
       </div>
     </TmaPage>
   )
