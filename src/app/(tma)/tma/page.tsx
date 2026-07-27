26c8fbb0657d6bd9cc2c2a4558d17363636688bf
Kennix88 <shel.live@yandex.ru>
2026-07-27 19:04:27 +0800

    Update page.tsx

--- src/app/(tma)/tma/page.tsx
+++ src/app/(tma)/tma/page.tsx
@@ -2,11 +2,8 @@
 import { TmaPage } from '@app/app/(tma)/_components/TmaPage'
 import { useFullscreenAd } from '@app/app/_components/ads/useFullscreenAd'
 import { PremiumCTA } from '@app/app/_components/PremiumCTA'
-import SocialButtons from '@app/app/_components/SocialButtons'
 import { Subscription } from '@app/app/_components/subscription/Subscription'
 import TopBar from '@app/app/_components/TopBar'
-import { Users2 } from 'lucide-react'
-import { SectionHeading } from './earning/_components/SectionHeading'
 
 export default function Page() {
   useFullscreenAd()
@@ -16,17 +13,6 @@ export default function Page() {
         <TopBar />
         <PremiumCTA />
         <Subscription />
-
-        {/*<SupportBanner />*/}
-        <br />
-        <div className="flex flex-col gap-2.5">
-          <SectionHeading
-            icon={<Users2 size={14} />}
-            title="Сообщество"
-            hint="Будь на связи с VPNsib"
-          />
-          <SocialButtons />
-        </div>
       </div>
     </TmaPage>
   )
